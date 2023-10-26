73b91b2e88b7e90289ee7cf22d21131c49ee96d0
Kohsei Takaoka <mira.fpsplayer@gmail.com>
2023-10-26 12:45:12 +0900

    Make a test human friendly

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SpDX-FileCopyrightText: 2023 Kohsei Takaoka ta.kousei0226@gmail.com
+# SPDX-License-Identifier:BSD-3-Clause
 
+ng () {
+	echo NG at LINE $1
+	res1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res

ed5e3f02c396b42902eca067f5ad8e680fa4bb9a
Kohsei Takaoka <mira.fpsplayer@gmail.com>
2023-11-24 18:06:43 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash -xv
+# SpDX-FileCopyrightText: 2023 Kohsei Takaoka ta.kousei0226@gmail.com
+# SPDX-License-Identifier:BSD-3-Clause
+
+ng () {
+        echo NG at LINE $1
+        res1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = "[1, 2, 3, 4, 5] = 15 [-5, -4, -3, -2, -1] = -15 [1, 2, 3, 4, 5] = 120 [1, 2, 3, 4, 5] = 0.008333" ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
